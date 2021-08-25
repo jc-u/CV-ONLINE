57a806edb8055df55b3861f65300620804ef4c2f
jc-u <jeanchristophe.urbain@viacesi.fr>
2021-08-25 16:48:39 +0200

    A propos + competences

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var toggle = document.querySelector("#toggle");
 var body = document.querySelector("body");
 var close = document.querySelector("#close");
+var li = document.querySelectorAll("#mobile li");
 
 toggle.addEventListener("click", function (e) {
   e.preventDefault();
@@ -10,3 +11,10 @@ toggle.addEventListener("click", function (e) {
 close.addEventListener("click", function () {
   body.classList.remove("with--sidebar");
 });
+
+for (let i = 0; i < li.length; i++) {
+  const element = li[i];
+  element.addEventListener("click", function () {
+    body.classList.remove("with--sidebar");
+  });
+}
